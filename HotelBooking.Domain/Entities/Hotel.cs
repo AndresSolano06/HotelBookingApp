aaa917415ce7d6eff2f74dfa02936443728d1817
@AndresSolano06 <andresolano.12.651@gmail.com>
2025-02-25 23:41:29 -0500

    Changes in model Hotel for data required

--- HotelBooking.Domain/Entities/Hotel.cs
+++ HotelBooking.Domain/Entities/Hotel.cs
@@ -1,13 +1,20 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HotelBooking.Domain.Entities
 {
     public class Hotel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The Address field is required.")]
         public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The City field is required.")]
         public string City { get; set; } = string.Empty;
+
         public bool IsActive { get; set; } = true;
         public List<Room> Rooms { get; set; } = new();
     }
